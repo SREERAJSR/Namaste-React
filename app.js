28ee8c9615a42ec48f46bb02b7cecce701f4a53b
SREERAJ <admin@SREERAJ-S-R-MacBook-Air.local>
2025-01-26 12:12:33 +0530

    added complex nest html elemnt using js in react

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
-const heading = React.createElement('h1', {id:'heading',xyz:'hai'}, 'Hello world using React');
-console.log(heading)
+
+/*
+
+<div id='parent'>
+
+<div id='child'>
+<h1>I am first child </h1>
+<h1>I am second child </h1>
+</div>
+<div id='child2'>
+<h1>I am second child </h1>
+</div>
+</div>
+
+*/
+const parent = React.createElement('div', { id: 'parent' },
+    [React.createElement('div', { id: 'child' },
+        [React.createElement('h1', {}, 'Iam first child'),
+            React.createElement('h2', {}, "Iam 2nd child ")]
+    ), React.createElement('div', { id: 'child2' },
+        [React.createElement('h1', {}, 'Iam first child'),
+        React.createElement('h2', {}, "Iam 2nd child ")]
+    )]
+)
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading)
+root.render(parent)
