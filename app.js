2746b004dbec4b61041250b8c0bb008ca0a6c84c
SREERAJ <admin@SREERAJ-S-R-MacBook-Air.local>
2025-01-26 22:55:49 +0530

    added parcel, installed react, reactdom library

--- app.js
+++ app.js
@@ -1,26 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-/*
 
-<div id='parent'>
-
-<div id='child'>
-<h1>I am first child </h1>
-<h1>I am second child </h1>
-</div>
-<div id='child2'>
-<h1>I am second child </h1>
-</div>
-</div>
-
-*/
 const parent = React.createElement('div', { id: 'parent' },
     [React.createElement('div', { id: 'child' },
         [React.createElement('h1', {}, 'Iam first child'),
             React.createElement('h2', {}, "Iam 2nd child ")]
-    ), React.createElement('div', { id: 'child2' },
-        [React.createElement('h1', {}, 'Iam first child'),
-        React.createElement('h2', {}, "Iam 2nd child ")]
+    ), React.createElement('div', { id: 'chld2' },
+        [React.createElement('h1', {}, 'I   first child'),
+        React.createElement('h2', {}, "I love you  nimisha")]
     )]
-)
+)  
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(parent)
