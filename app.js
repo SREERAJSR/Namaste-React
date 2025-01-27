93eefe0cd29e3031a0ed5db053d9921715ce7ecf
SREERAJ <admin@SREERAJ-S-R-MacBook-Air.local>
2025-01-27 14:01:34 +0530

    created react functional component

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+// React element
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Namaste React 🚀"
+);
+// jsx => babel transplied it into react element
+const nav = <h1>Hello from jsx, react element</h1>
 
-const parent = React.createElement('div', { id: 'parent' },
-    [React.createElement('div', { id: 'child' },
-        [React.createElement('h1', {}, 'Iam first child'),
-            React.createElement('h2', {}, "Iam 2nd child ")]
-    ), React.createElement('div', { id: 'chld2' },
-        [React.createElement('h1', {}, 'I   first child'),
-        React.createElement('h2', {}, "I love you  nimisha")]
-    )]
-)  
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent)
+// React component
+const TitleComponent =()=>(
+  <h1>Hello from title comp</h1>
+)
+const HeadingComponent = () => {
+  return (<div id='heading'>
+    <TitleComponent />
+  <h1>Namaste React from Heading comp</h1>
+  </div>);
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent/>);
