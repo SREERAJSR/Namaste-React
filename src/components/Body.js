6021d99e76a65de398c6215b4212ce0b048d0c0c
SREERAJ <admin@SREERAJ-S-R-MacBook-Air.local>
2025-02-02 20:58:15 +0530

    added search functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,33 +4,44 @@ import Shimmer  from "./Shimmer";
 import { useState,useEffect } from "react";
 const Body = () => {
   const [resData, setresData] = useState([]);
+  const [filteredResData,setFilteredResData] = useState([])
+  const [searchText,setSearchText] = useState('')
 
  useEffect(()=>{
-  fetchData()
+    fetchData()
  },[])
 
  async function fetchData(){
   const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=10.51600&lng=76.21570&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING')
   const json = await data.json()
-setresData(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
-
+const actualData =json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+setresData(actualData)
+setFilteredResData(actualData)
  }
 //conditional rendering
 return resData.length===0?<Shimmer/>:(
     <div className="body">
       <div className="filter-container">
+        <input className="search-input" value={searchText} onChange={(e)=> {
+          setSearchText(e.target.value)}
+          
+          } ></input>
+        <button  onClick={()=>{
+          const filteredData =resData.filter((res)=> res.info.name.toLowerCase().includes(searchText.toLowerCase()))
+          setFilteredResData(filteredData)
+        }} >search</button>
         <button
           className="filter-btn"
           onClick={() => {
-            filteredData = resData.filter((res) => res.info.avgRating >4);
-            setresData(filteredData);
+             const filteredData = resData.filter((res) => res.info.avgRating <4.5);
+            setFilteredResData(filteredData);
           }}
         >
           Top rated Restuarants
         </button>
       </div>
       <div className="res-container">
-        {resData.map((res) => (
+        {filteredResData.map((res) => (
           <RestaurantCard id={res.info.id} resData={res} />
         ))}
       </div>
